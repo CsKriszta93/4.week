a725d9664b644b2fd63c5370a6f1d8a3cacf0807
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-25 13:30:30 +0200

    finished Post-it exercise

---
+++ day1/Post-It/Post-It/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Post_It
+{
+    public struct Postit
+    {
+        public string BackgroundColor;
+        public string Text;
+        public string TextColor;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Postit postit1 = new Postit();
+            postit1.BackgroundColor = "orange";
+            postit1.TextColor = "blue";
+            postit1.Text = "Idea 1";
+
+            Postit postit2 = new Postit();
+            postit2.BackgroundColor = "pink";
+            postit2.TextColor = "black";
+            postit2.Text = "awesome";
+
+            Postit postit3 = new Postit();
+            postit3.BackgroundColor = "yellow";
+            postit3.TextColor = "green";
+            postit3.Text = "Superb";
+        }
+    }
+}
