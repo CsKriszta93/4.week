4969ee5395d9c00381e0ee286d8ca3005fc34f41
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-26 16:41:14 +0200

    Student class created

--- day2/Greenfox/Greenfox/Person.cs
+++ day2/Greenfox/Greenfox/Person.cs
@@ -40,14 +40,41 @@ namespace Greenfox
             this.name = name;
             this.age = age;
             this.gender = gender;
-    
+
         }
+    }
 
     class Student : Person
     {
         public string previousOrganization;
         public int skippedDays;
-    }
 
+        public new void GetGoal()
+        {
+            Console.WriteLine("Be a junior software developer.");
+        }
+
+        public void Introduce()
+        {
+                Console.WriteLine("Hi, I'm {0}, a {1} year old {2} from {3}, who skipped {4} days from the course already.", name, age, gender, previousOrganization, skippedDays);
+        }
+
+        public Student(string name, int age, string gender, string previousOrganization)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.previousOrganization = previousOrganization;
+            skippedDays = 0;
+        }
+
+        public Student()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+            previousOrganization = "School Of Life";
+            skippedDays = 0;
+        }
     }
 }
