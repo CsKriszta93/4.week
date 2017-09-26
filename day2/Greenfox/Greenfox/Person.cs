98e7ea4701d377ab8aaf264817262299973a0ff0
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-26 16:08:25 +0200

    day2 exercises

---
+++ day2/Greenfox/Greenfox/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greenfox
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public string gender;
+
+        public void Introduce(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+
+            Console.WriteLine("Hi, I'm {0} , a {1} year old {2}.", name, age, gender);
+
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment.");
+
+        }
+
+        public Person()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+        }
+
+        public Person(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+    
+        }
+
+    }
+}
