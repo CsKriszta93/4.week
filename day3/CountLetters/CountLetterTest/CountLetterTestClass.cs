5a94493fc0a7dc998dd626751f0b063df74206d6
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-27 17:13:03 +0200

    CountLetters Test finished

---
+++ day3/CountLetters/CountLetterTest/CountLetterTestClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CountLetters;
+
+
+namespace CountLetterTest
+{
+    [TestFixture]
+    public class CountLetterTestClass
+    {
+        [Test]
+        public void CountLetterTest()
+        {
+            CountLettersClass lettertest = new CountLettersClass();
+            string input = "blablabbla";
+            Dictionary<char, int> output = new Dictionary<char, int>();
+            output.Add('b', 4);
+            output.Add('l', 3);
+            output.Add('a', 3);
+
+            Assert.AreEqual(output, lettertest.CountLetter(input));
+
+        }
+    }
+}
---
+++ day3/CountLetters/CountLetters/CountLettersClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CountLetters
+{
+    public class CountLettersClass
+    {
+        public Dictionary<char, int> CountLetter(string input)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            char[] inputChar = input.ToCharArray();
+            foreach (char letter in inputChar)
+            {
+                if (!dictionary.ContainsKey(letter))
+                {
+                    dictionary.Add(letter, 1);
+                }
+                else
+                {
+                    dictionary[letter]++;
+                }
+                
+            }
+            return dictionary;
+        }
+
+    }
+}
