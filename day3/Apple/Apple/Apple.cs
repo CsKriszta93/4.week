b64f0bb8084813bd25524176dae7244801c30bf4
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-27 13:28:23 +0200

    Apple Test finished

---
+++ day3/Apple/Apple/Apple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apple
+{
+    public class Apple
+    {
+        public string apple;
+        public void GetApple(string apple)
+        {
+            this.apple = apple;
+        }
+
+        public string Printapple()
+        {
+            apple = "apple";
+            Console.WriteLine(apple);
+            return apple;
+        }
+    }
+}
---
+++ day3/Apple/AppleTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Apple;
+
+namespace Apple.Test
+{
+    [TestFixture]
+    public class AppleTest
+    {
+        [Test]
+        public void GetApple()
+        {
+            string apple = "apple";
+            var Appletest = new Apple();
+            string output = Appletest.Printapple();
+            Assert.AreEqual(apple, output);
+        }
+    }
+}
