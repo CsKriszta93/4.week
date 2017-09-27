8ecd8a71b362e389abed9d5a007d2ef86cf6b259
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-27 14:25:33 +0200

    First test with list finished

---
+++ day3/Sum/Sum/SumClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class SumClass
+    {
+        public int Summy(List<int> numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
---
+++ day3/Sum/SumTest/SumTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Sum;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SumTest
+    {
+        [Test]
+        public void PrintSumTest()
+        {
+         SumClass summ = new SumClass();
+            var listie = new List<int> { 3, 3, 4, 5, 6, 6 };
+            int output = 27;
+            Assert.AreEqual(output, summ.Summy(listie));
+        }
+    }
+}
