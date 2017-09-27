a1109d5ec2c82517bd125c2dc793e24be38b4b86
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-27 15:30:41 +0200

    AnagramTest finished

---
+++ day3/Anagram/Anagram/AnagramClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class AnagramClass
+    {
+        public bool AnagramTester(string input1, string input2)
+        {
+            char[] char1 = input1.ToCharArray();
+            char[] char2 = input2.ToCharArray();
+            Array.Sort(char1);
+            Array.Sort(char2);
+
+            if (char1.Length != char2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < char1.Length; i++)
+            {
+                if (char1[i] != char2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ day3/Anagram/AnagramTest/AnagramTestClass.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramTestClass
+    {
+        [Test]
+        public void AnagramTesterTest()
+        {
+            AnagramClass anagram = new AnagramClass();
+            bool output = true;
+            string input1 = "dog";
+            string input2 = "god";
+            Assert.AreEqual(output, anagram.AnagramTester(input1, input2));
+        }
+    }
+}
