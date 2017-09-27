d0622cc82fd6dfd71b6428c3e693d7e844605e9a
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-27 13:42:16 +0200

    create Sum funtion

---
+++ day3/Sum/Sum/Sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class Sum
+    {
+        public int Sum(List<int> numbers) 
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
